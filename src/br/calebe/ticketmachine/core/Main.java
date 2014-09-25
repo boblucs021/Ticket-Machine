ade8a5f385ff17bcd70a756a99481aec9ed4e3bc
31252631 <31252631@089722-P33S08.macklabs.br>
2014-09-25 19:41:58 -0300

    Enviando ao GitHub

---
+++ src/br/calebe/ticketmachine/core/Main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.calebe.ticketmachine.core;
+
+/**
+ *
+ * @author 31252631
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Ticket Machine!!");
+    }
+}
